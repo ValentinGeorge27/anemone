637a47693e8d1a13a3c866764eea90eb8203dceb
ValentinGeorge27 <george.valentin27@gmail.com>
2015-06-28 13:37:15 +0300

    rona

--- lib/anemone/core.rb
+++ lib/anemone/core.rb
@@ -270,7 +270,7 @@ module Anemone
     end
 
     def in_allowed_subdomain?(link)
-      opts[:crawl_subdomains] and @valid_domains.find{|domain| PublicSuffix.parse(URI.parse(URI.encode(link.to_s)).host).domain.eql?(domain)}
+      opts[:crawl_subdomains] and @valid_domains.find{|domain| link.host.end_with?(domain)}
     end
 
     #
